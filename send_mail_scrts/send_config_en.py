3fdc9f96f4877e25b411f6cd6f8a8be858f80d47
rgregor <jbez0rg@protonmail.com>
2024-02-25 16:13:24 +0100

    Initial commit 20240225_1613_d

---
+++ send_mail_scrts/send_config_en.py
@@ -0,0 +1,12 @@
+import os
+# define email variables:
+# Header info:
+gSmtp  = os.getenv("SMTP_SCRTS")
+gPort  = os.getenv("PORT_SCRTS")
+gFrom  = os.getenv("FROM_SCRTS")
+gTo    = ['gredelonghi@gmail.com']
+
+# Credentals:
+gUnm   = os.getenv("UNM_SCRTS")
+gPsswd = os.getenv("PSSWD_SCRTS")
+
