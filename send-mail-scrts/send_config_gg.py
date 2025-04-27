73537d1ef0aa922165ad9c89c103a886d4c95e5b
yb3_z0rg <yb3_z0rg@proton.me>
2025-04-27 12:19:15 +0200

    update 20250427_1219_jbe

--- send-mail-scrts/send_config_gg.py
+++ send-mail-scrts/send_config_gg.py
@@ -3,6 +3,7 @@ import os
 # Header info:
 gFrom = os.getenv("FROM_SCRTS_GG")
 gTo = ['gregor.redelonghi@energetika.si']
+# gTo = ['gredelonghi@protonmail.com']
 gSmtp = os.getenv("SMTP_SCRTS_GG")
 gPort = os.getenv("PORT_SCRTS_GG")
 
